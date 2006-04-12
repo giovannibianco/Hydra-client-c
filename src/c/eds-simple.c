ac8bd92ab260cbdd1f0adc9c79d8c7c9e9f52f20
szamsu <szamsu@4c42d608-eb89-46b6-9e62-40a981e1fec6>
2006-04-12 13:23:51 +0000

    soft-tabs

--- src/c/eds-simple.c
+++ src/c/eds-simple.c
@@ -6,8 +6,8 @@
  *  GLite Data Management - Simple encrypted data storage API implementation
  *
  *  Authors:
- *	Zoltan Farkas <Zoltan.Farkas@cern.ch>
- *	Patrick Guio <patrick.guio@bccs.uib.no>
+ *        Zoltan Farkas <Zoltan.Farkas@cern.ch>
+ *        Patrick Guio <patrick.guio@bccs.uib.no>
  *
  */
 
@@ -31,7 +31,7 @@
 #define EDS_DEFAULT_CIPHER "bf-cbc"
 
 EVP_CIPHER_CTX *glite_eds_init(char *id, char **key, char **iv,
-			       const EVP_CIPHER **type, char **error);
+                               const EVP_CIPHER **type, char **error);
 
 
 /**
@@ -46,15 +46,15 @@ static char *get_attr_value(glite_catalog_Attribute **attrs, int attrnum,
 
     for (i = 0; i < attrnum; i++)
     {
-	if (!strcmp(attrs[i]->name, name))
-	    if (attrs[i]->value)
-	    {
-		retval = strdup(attrs[i]->value);
-		break;
-	    }
+        if (!strcmp(attrs[i]->name, name))
+            if (attrs[i]->value)
+            {
+                retval = strdup(attrs[i]->value);
+                break;
+            }
     }
     if (!retval && def_val)
-	retval = strdup(def_val);
+        retval = strdup(def_val);
 
     return retval;
 }
@@ -69,10 +69,10 @@ static int to_hex(unsigned char *in, int insize, unsigned char **out)
     const char hex[] = "0123456789ABCDEF";
     
     if (!out)
-	return ret;
+        return ret;
 
     if (NULL == (*out = (unsigned char *)calloc(1, insize*2+1)))
-	return ret;
+        return ret;
     ret = insize * 2;
 
     for (i = 0; i < insize; i++)
@@ -103,10 +103,10 @@ static int to_bin(unsigned char *in, unsigned char **out)
     for (i = 0; i < ret; i++)
     {
         char *p;
-	p = strchr(hex, in[i*2]);
-	(*out)[i] = (p-hex)*16;
-	p = strchr(hex, in[i*2+1]);
-	(*out)[i] += (p-hex);
+        p = strchr(hex, in[i*2]);
+        (*out)[i] = (p-hex)*16;
+        p = strchr(hex, in[i*2+1]);
+        (*out)[i] += (p-hex);
     }
 
     return ret;
@@ -124,31 +124,31 @@ int glite_eds_put_metadata(char *id, char *hex_key, char *hex_iv, char *cipher,
     const glite_catalog_Attribute cipher_attr = {EDS_ATTR_CIPHER, cipher, NULL};
     const glite_catalog_Attribute keyinfo_attr = {EDS_ATTR_KEYINFO, keyinfo, NULL};
     const glite_catalog_Attribute *attrs[] = {&cipher_attr, &key_attr,
-	&iv_attr, &keyinfo_attr};
+        &iv_attr, &keyinfo_attr};
 
     if (NULL == (ctx = glite_catalog_new(NULL)))
     {
-	const char *catalog_err;
-	catalog_err = glite_catalog_get_error(ctx);
+        const char *catalog_err;
+        catalog_err = glite_catalog_get_error(ctx);
 
-	asprintf(error, "glite_eds_put_metadata error: %s", catalog_err);
-	return -1;
+        asprintf(error, "glite_eds_put_metadata error: %s", catalog_err);
+        return -1;
     }
     if (glite_metadata_createEntry(ctx, id, "eds"))
     {
-	const char *catalog_err;
-	catalog_err = glite_catalog_get_error(ctx);
+        const char *catalog_err;
+        catalog_err = glite_catalog_get_error(ctx);
 
-	asprintf(error, "glite_eds_put_metadata error: %s", catalog_err);
-	return -1;
+        asprintf(error, "glite_eds_put_metadata error: %s", catalog_err);
+        return -1;
     }
     if (glite_metadata_setAttributes(ctx, id, 4, attrs))
     {
-	const char *catalog_err;
-	catalog_err = glite_catalog_get_error(ctx);
+        const char *catalog_err;
+        catalog_err = glite_catalog_get_error(ctx);
 
-	asprintf(error, "glite_eds_put_metadata error: %s", catalog_err);
-	return -1;
+        asprintf(error, "glite_eds_put_metadata error: %s", catalog_err);
+        return -1;
     }
     glite_catalog_free(ctx);
 
@@ -171,51 +171,51 @@ EVP_CIPHER_CTX *glite_eds_init(char *id, char **key, char **iv,
     /* Get Metadata Catalog attributes for the file */
     if (NULL == (ctx = glite_catalog_new(NULL)))
     {
-	const char *catalog_err;
-	catalog_err = glite_catalog_get_error(ctx);
+        const char *catalog_err;
+        catalog_err = glite_catalog_get_error(ctx);
 
-	asprintf(error, "glite_eds_init error: %s", catalog_err);
-	return NULL;
+        asprintf(error, "glite_eds_init error: %s", catalog_err);
+        return NULL;
     }
 
     result = glite_metadata_getAttributes(ctx, id, 4, attrs, &result_cnt);
     if (result_cnt < 0)
     {
-	const char *catalog_err;
-	catalog_err = glite_catalog_get_error(ctx);
+        const char *catalog_err;
+        catalog_err = glite_catalog_get_error(ctx);
 
-	asprintf(error, "glite_eds_init error: %s", catalog_err);
-	return NULL;
+        asprintf(error, "glite_eds_init error: %s", catalog_err);
+        return NULL;
     }
 
     /* Check the presence of all required attributes */
     cipher_name = get_attr_value(result, result_cnt, EDS_ATTR_CIPHER, NULL);
     if (!cipher_name)
     {
-	asprintf(error, "glite_eds_init error: attribute %s not found "
-	    "in metadata catalog", EDS_ATTR_CIPHER);
-	return NULL;
+        asprintf(error, "glite_eds_init error: attribute %s not found "
+            "in metadata catalog", EDS_ATTR_CIPHER);
+        return NULL;
     }
     hex_iv = get_attr_value(result, result_cnt, EDS_ATTR_IV, NULL);
     if (!hex_iv)
     {
-	asprintf(error, "glite_eds_init error: attribute %s not found "
-	    "in metadata catalog", EDS_ATTR_IV);
-	return NULL;
+        asprintf(error, "glite_eds_init error: attribute %s not found "
+            "in metadata catalog", EDS_ATTR_IV);
+        return NULL;
     }
     hex_key = get_attr_value(result, result_cnt, EDS_ATTR_KEY, NULL);
     if (!hex_key)
     {
-	asprintf(error, "glite_eds_init error: attribute %s not found "
-	    "in metadata catalog", EDS_ATTR_KEY);
-	return NULL;
+        asprintf(error, "glite_eds_init error: attribute %s not found "
+            "in metadata catalog", EDS_ATTR_KEY);
+        return NULL;
     }
     keyinfo = get_attr_value(result, result_cnt, EDS_ATTR_KEYINFO, NULL);
     if (!keyinfo)
     {
-	asprintf(error, "glite_eds_init error: attribute %s not found "
-	    "in metadata catalog", EDS_ATTR_KEYINFO);
-	return NULL;
+        asprintf(error, "glite_eds_init error: attribute %s not found "
+            "in metadata catalog", EDS_ATTR_KEYINFO);
+        return NULL;
     }
     glite_catalog_Attribute_freeArray(ctx, result_cnt, result);
     glite_catalog_free(ctx);
@@ -225,23 +225,23 @@ EVP_CIPHER_CTX *glite_eds_init(char *id, char **key, char **iv,
 
     if (!RAND_load_file("/dev/random", 1))
     {
-	asprintf(error, "glite_eds_init error: %s",
-	    ERR_error_string(ERR_get_error(), NULL));
-	return NULL;
+        asprintf(error, "glite_eds_init error: %s",
+            ERR_error_string(ERR_get_error(), NULL));
+        return NULL;
     }
     OpenSSL_add_all_ciphers();
     if (0 == (*type = EVP_get_cipherbyname(cipher_name)))
     {
-	asprintf(error, "glite_eds_init error: %s",
-	    ERR_error_string(ERR_get_error(), NULL));
-	return NULL;
+        asprintf(error, "glite_eds_init error: %s",
+            ERR_error_string(ERR_get_error(), NULL));
+        return NULL;
     }
 
     if (NULL == (ectx = (EVP_CIPHER_CTX *)calloc(1, sizeof(*ectx))))
     {
-	asprintf(error, "glite_eds_init error: calloc() of %d "
-	    "bytes failed", sizeof(*ectx));
-	return NULL;
+        asprintf(error, "glite_eds_init error: calloc() of %d "
+            "bytes failed", sizeof(*ectx));
+        return NULL;
     }
     EVP_CIPHER_CTX_init(ectx);
 
@@ -263,17 +263,17 @@ int glite_eds_register(char *id, char *cipher, int keysize,
     /* Do OpenSSL cipher initialization */
     if (!RAND_load_file("/dev/random", 1))
     {
-	asprintf(error, "glite_eds_register error: %s",
-	    ERR_error_string(ERR_get_error(), NULL));
-	return -1;
+        asprintf(error, "glite_eds_register error: %s",
+            ERR_error_string(ERR_get_error(), NULL));
+        return -1;
     }
     OpenSSL_add_all_ciphers();
     cipher_to_use = (cipher) ? cipher : EDS_DEFAULT_CIPHER;
     if (0 == (type = EVP_get_cipherbyname(cipher_to_use)))
     {
-	asprintf(error, "glite_eds_register error: %s",
-	    ERR_error_string(ERR_get_error(), NULL));
-	return -1;
+        asprintf(error, "glite_eds_register error: %s",
+            ERR_error_string(ERR_get_error(), NULL));
+        return -1;
     }
 
     /* Initialize encryption key and initialization vector */
@@ -281,45 +281,45 @@ int glite_eds_register(char *id, char *cipher, int keysize,
     keyLength = (keysize) ? (keysize >> 3) : EVP_CIPHER_key_length(type);
     if (NULL == (iv = (char *)malloc(ivLength)))
     {
-	asprintf(error, "glite_eds_register error: malloc() of %d bytes "
-	    "failed", ivLength);
-	return -1;
+        asprintf(error, "glite_eds_register error: malloc() of %d bytes "
+            "failed", ivLength);
+        return -1;
     }
     if (NULL == (key = (char *)malloc(keyLength)))
     {
-	asprintf(error, "glite_eds_register error: malloc() of %d bytes "
-	    "failed", keyLength);
-	return -1;
+        asprintf(error, "glite_eds_register error: malloc() of %d bytes "
+            "failed", keyLength);
+        return -1;
     }
     RAND_bytes((char *)key, keyLength);
     if (keyLength * 2 != to_hex(key, keyLength, (unsigned char **)&hex_key))
     {
         asprintf(error, "glite_eds_register error: converting key to hex "
-	    "format failed");
-	return -1;
+            "format failed");
+        return -1;
     }
     RAND_pseudo_bytes((char *)iv, ivLength);
     if (ivLength * 2 != to_hex(iv, ivLength, (unsigned char **)&hex_iv))
     {
         asprintf(error, "glite_eds_register error: converting iv to hex "
-	    "format failed");
-	return -1;
+            "format failed");
+        return -1;
     }
 
     /* Do the Metadata Catalog stuff */
     asprintf(&keyl_str, "%d", keyLength<<3);
     if (glite_eds_put_metadata(id, hex_key, hex_iv, cipher_to_use, keyl_str, error))
     {
-	return -1;
+        return -1;
     }
 
     /* If id (SURL/GUID) is present, create Fireman Catalog entry
     if (id)
     {
         if (glite_eds_put_fireman(lfn, id, error))
-	{
-	    return -1;
-	}
+        {
+            return -1;
+        }
     }
     */
 
@@ -343,24 +343,24 @@ EVP_CIPHER_CTX *glite_eds_register_encrypt_init(char *id,
     /* Do OpenSSL cipher initialization */
     if (!RAND_load_file("/dev/random", 1))
     {
-	asprintf(error, "glite_eds_register_encrypt_init error: %s",
-	    ERR_error_string(ERR_get_error(), NULL));
-	return NULL;
+        asprintf(error, "glite_eds_register_encrypt_init error: %s",
+            ERR_error_string(ERR_get_error(), NULL));
+        return NULL;
     }
     OpenSSL_add_all_ciphers();
     cipher_to_use = (cipher) ? cipher : EDS_DEFAULT_CIPHER;
     if (0 == (type = EVP_get_cipherbyname(cipher_to_use)))
     {
-	asprintf(error, "glite_eds_register_encrypt_init error: %s",
-	    ERR_error_string(ERR_get_error(), NULL));
-	return NULL;
+        asprintf(error, "glite_eds_register_encrypt_init error: %s",
+            ERR_error_string(ERR_get_error(), NULL));
+        return NULL;
     }
 
     if (NULL == (ectx = (EVP_CIPHER_CTX *)calloc(1, sizeof(*ectx))))
     {
-	asprintf(error, "glite_eds_register_encrypt_init error: calloc() of %d "
-	    "bytes failed", sizeof(*ectx));
-	return NULL;
+        asprintf(error, "glite_eds_register_encrypt_init error: calloc() of %d "
+            "bytes failed", sizeof(*ectx));
+        return NULL;
     }
     EVP_CIPHER_CTX_init(ectx);
     EVP_EncryptInit(ectx, type, NULL, NULL);
@@ -370,34 +370,34 @@ EVP_CIPHER_CTX *glite_eds_register_encrypt_init(char *id,
     keyLength = (keysize) ? (keysize >> 3) : EVP_CIPHER_key_length(type);
     if (NULL == (iv = (char *)malloc(ivLength)))
     {
-	asprintf(error, "glite_eds_register_encrypt_init error: malloc() of %d "
-	    "bytes failed", ivLength);
-	free(ectx);
-	return NULL;
+        asprintf(error, "glite_eds_register_encrypt_init error: malloc() of %d "
+            "bytes failed", ivLength);
+        free(ectx);
+        return NULL;
     }
     if (NULL == (key = (char *)malloc(keyLength)))
     {
-	asprintf(error, "glite_eds_register_encrypt_init error: malloc() of %d "
-	    "bytes failed", keyLength);
-	free(ectx);
-	return NULL;
+        asprintf(error, "glite_eds_register_encrypt_init error: malloc() of %d "
+            "bytes failed", keyLength);
+        free(ectx);
+        return NULL;
     }
     RAND_bytes((char *)key, keyLength);
 
     if (keyLength * 2 != to_hex(key, keyLength, (unsigned char **)&hex_key))
     {
         asprintf(error, "glite_eds_register_encrypt_init error: converting key to hex "
-	    "format failed");
-	free(ectx);
-	return NULL;
+            "format failed");
+        free(ectx);
+        return NULL;
     }
     RAND_pseudo_bytes((char *)iv, ivLength);
     if (ivLength * 2 != to_hex(iv, ivLength, (unsigned char **)&hex_iv))
     {
         asprintf(error, "glite_eds_register_encrypt_init error: converting iv to hex "
-	    "format failed");
-	free(ectx);
-	return NULL;
+            "format failed");
+        free(ectx);
+        return NULL;
     }
 
     EVP_EncryptInit(ectx, NULL, key, iv);
@@ -407,7 +407,7 @@ EVP_CIPHER_CTX *glite_eds_register_encrypt_init(char *id,
     if (glite_eds_put_metadata(id, hex_key, hex_iv, cipher_to_use, keyl_str, error))
     {
         free(ectx);
-	return NULL;
+        return NULL;
     }
     free(keyl_str);
 
@@ -426,7 +426,7 @@ EVP_CIPHER_CTX *glite_eds_encrypt_init(char *id, char **error)
     
     if (NULL == (ectx = glite_eds_init(id, &key, &iv, &type, error)))
     {
-	return NULL;
+        return NULL;
     }
 
     EVP_EncryptInit(ectx, type, key, iv);
@@ -447,7 +447,7 @@ EVP_CIPHER_CTX *glite_eds_decrypt_init(char *id, char **error)
     
     if (NULL == (dctx = glite_eds_init(id, &key, &iv, &type, error)))
     {
-	return NULL;
+        return NULL;
     }
 
     EVP_DecryptInit(dctx, type, key, iv);
@@ -468,13 +468,13 @@ int glite_eds_encrypt_block(EVP_CIPHER_CTX *ectx, char *mem_in, int mem_in_size,
     enc_buffer = (char *)malloc(mem_in_size + EVP_CIPHER_CTX_block_size(ectx));
     if (!enc_buffer)
     {
-	asprintf(error, "glite_eds_encrypt_block error: failed to allocate "
-	    "%d bytes of memory", mem_in_size + EVP_CIPHER_CTX_block_size(ectx));
-	return -1;
+        asprintf(error, "glite_eds_encrypt_block error: failed to allocate "
+            "%d bytes of memory", mem_in_size + EVP_CIPHER_CTX_block_size(ectx));
+        return -1;
     }
 
     EVP_EncryptUpdate(ectx, enc_buffer, &enc_buffer_size,
-	mem_in, mem_in_size);
+        mem_in, mem_in_size);
 
     *mem_out = enc_buffer;
     *mem_out_size = enc_buffer_size;
@@ -493,9 +493,9 @@ int glite_eds_encrypt_final(EVP_CIPHER_CTX *ectx, char **mem_out, int *mem_out_s
     enc_buffer = (char *)malloc(EVP_CIPHER_CTX_block_size(ectx));
     if (!enc_buffer)
     {
-	asprintf(error, "glite_eds_encrypt_final error: failed to allocate "
-	    "%d bytes of memory", EVP_CIPHER_CTX_block_size(ectx));
-	return -1;
+        asprintf(error, "glite_eds_encrypt_final error: failed to allocate "
+            "%d bytes of memory", EVP_CIPHER_CTX_block_size(ectx));
+        return -1;
     }
 
     EVP_EncryptFinal(ectx, enc_buffer, &enc_buffer_size);
@@ -518,13 +518,13 @@ int glite_eds_decrypt_block(EVP_CIPHER_CTX *dctx, char *mem_in,  int mem_in_size
     dec_buffer = (char *)malloc(mem_in_size + EVP_CIPHER_CTX_block_size(dctx));
     if (!dec_buffer)
     {
-	asprintf(error, "glite_eds_decrypt_block error: failed to allocate "
-	    "%d bytes of memory", mem_in_size + EVP_CIPHER_CTX_block_size(dctx));
-	return -1;
+        asprintf(error, "glite_eds_decrypt_block error: failed to allocate "
+            "%d bytes of memory", mem_in_size + EVP_CIPHER_CTX_block_size(dctx));
+        return -1;
     }
 
     EVP_DecryptUpdate(dctx, dec_buffer, &dec_buffer_size,
-	mem_in, mem_in_size);
+        mem_in, mem_in_size);
 
     *mem_out = dec_buffer;
     *mem_out_size = dec_buffer_size;
@@ -543,9 +543,9 @@ int glite_eds_decrypt_final(EVP_CIPHER_CTX *dctx, char **mem_out, int *mem_out_s
     dec_buffer = (char *)malloc(EVP_CIPHER_CTX_block_size(dctx));
     if (!dec_buffer)
     {
-	asprintf(error, "glite_eds_decrypt_final error: failed to allocate "
-	    "%d bytes of memory", EVP_CIPHER_CTX_block_size(dctx));
-	return -1;
+        asprintf(error, "glite_eds_decrypt_final error: failed to allocate "
+            "%d bytes of memory", EVP_CIPHER_CTX_block_size(dctx));
+        return -1;
     }
 
     EVP_DecryptFinal(dctx, dec_buffer, &dec_buffer_size);
@@ -573,20 +573,20 @@ int glite_eds_unregister(char *id, char **error)
     glite_catalog_ctx *ctx;
     if (NULL == (ctx = glite_catalog_new(NULL)))
     {
-	const char *catalog_err;
-	catalog_err = glite_catalog_get_error(ctx);
+        const char *catalog_err;
+        catalog_err = glite_catalog_get_error(ctx);
 
-	asprintf(error, "glite_eds_unregister error: %s", catalog_err);
-	return -1;
+        asprintf(error, "glite_eds_unregister error: %s", catalog_err);
+        return -1;
     }
 
     if (glite_metadata_removeEntry(ctx, id))
     {
-	const char *catalog_err;
-	catalog_err = glite_catalog_get_error(ctx);
+        const char *catalog_err;
+        catalog_err = glite_catalog_get_error(ctx);
 
-	asprintf(error, "glite_eds_unregister error: %s", catalog_err);
-	return -1;
+        asprintf(error, "glite_eds_unregister error: %s", catalog_err);
+        return -1;
     }
 
     return 0;
@@ -607,46 +607,46 @@ int glite_eds_put_fireman(char *lfn, char *id, char **error)
  
     if (NULL == (ctx = glite_catalog_new(NULL)))
     {
-	const char *catalog_err;
-	catalog_err = glite_catalog_get_error(ctx);
+        const char *catalog_err;
+        catalog_err = glite_catalog_get_error(ctx);
 
-	asprintf(error, "glite_eds_put_fireman error: %s", catalog_err);
-	return -1;
+        asprintf(error, "glite_eds_put_fireman error: %s", catalog_err);
+        return -1;
     }
 
     if (!strncmp(id, SURL_prefix, strlen(SURL_prefix)))
     {
-	glite_catalog_SURLEntry *surl_entry;
-	glite_catalog_FRCEntry *rentry;
-	
-	surl_entry = glite_catalog_SURLEntry_new(NULL, id, 1);
-	rentry = glite_catalog_FRCEntry_new(ctx, lfn);
-	glite_catalog_FRCEntry_addSurl(ctx, rentry, surl_entry);
-	
-	if (glite_fireman_create(ctx, rentry))
-	{
-	    const char *catalog_err;
-	    catalog_err = glite_catalog_get_error(ctx);
-	    
-	    asprintf(error, "glite_eds_put_fireman error: %s", catalog_err);
-	    return -1;
-	}
+        glite_catalog_SURLEntry *surl_entry;
+        glite_catalog_FRCEntry *rentry;
+        
+        surl_entry = glite_catalog_SURLEntry_new(NULL, id, 1);
+        rentry = glite_catalog_FRCEntry_new(ctx, lfn);
+        glite_catalog_FRCEntry_addSurl(ctx, rentry, surl_entry);
+        
+        if (glite_fireman_create(ctx, rentry))
+        {
+            const char *catalog_err;
+            catalog_err = glite_catalog_get_error(ctx);
+            
+            asprintf(error, "glite_eds_put_fireman error: %s", catalog_err);
+            return -1;
+        }
     }
     else
     {
- 	glite_catalog_FRCEntry *entry;
-	
-	entry = glite_catalog_FRCEntry_new(ctx, lfn);
-	glite_catalog_FRCEntry_setGuid(ctx, entry, id);
+         glite_catalog_FRCEntry *entry;
+        
+        entry = glite_catalog_FRCEntry_new(ctx, lfn);
+        glite_catalog_FRCEntry_setGuid(ctx, entry, id);
 
- 	if (glite_fireman_create(ctx, entry))
-	{
-	    const char *catalog_err;
-	    catalog_err = glite_catalog_get_error(ctx);
-	    
-	    asprintf(error, "glite_eds_put_fireman error: %s", catalog_err);
-	    return -1;
-	}
+         if (glite_fireman_create(ctx, entry))
+        {
+            const char *catalog_err;
+            catalog_err = glite_catalog_get_error(ctx);
+            
+            asprintf(error, "glite_eds_put_fireman error: %s", catalog_err);
+            return -1;
+        }
     }
 
     glite_catalog_free(ctx);
