82e1ad19a78ce3c4633d5dfcb438f780ea51b6a9
szamsu <szamsu@4c42d608-eb89-46b6-9e62-40a981e1fec6>
2006-09-18 16:30:34 +0000

    Introduced more logging messages for performance investigation

--- src/utils/eds-unlinkfile.cxx
+++ src/utils/eds-unlinkfile.cxx
@@ -41,7 +41,7 @@ void print_usage_and_die(FILE * out){
     fprintf (out, "<%s> Version %s by %s\n", PROGNAME, PACKAGE_VERSION, PROGAUTHOR);
     fprintf (out, "usage: %s <remotefilename> [-i <id>]\n", PROGNAME);
     fprintf (out, "  -i <id>        : the ID to use to look up the decryption key of this file "
-	    "(defaults to the remotefilename's GUID).\n");
+        "(defaults to the remotefilename's GUID).\n");
     fprintf (out, " Optional parameters:\n");
     fprintf (out, "  -v      : verbose mode\n");
     fprintf (out, "  -q      : quiet mode\n");
@@ -66,28 +66,28 @@ int main(int argc, char* argv[]) {
     bool silent     = false;
     
     int flag;
-    while ((flag = getopt (argc, argv, "qhv")) != -1) {
+    while ((flag = getopt (argc, argv, "qhvi:s:")) != -1) {
         switch (flag) {
             case 'q':
                 silent = true;
-		unsetenv(TOOL_USER_VERBOSE);
-        	break;
+                unsetenv(TOOL_USER_VERBOSE);
+                break;
             case 'h':
                 print_usage_and_die(stdout);
-        	break;
-	    case 's':
-		service_endpoint = strdup(optarg);
-		break;
-	    case 'i':
-		id = strdup(optarg);
-		break;
-	    case 'v':
-		silent = false;
-		setenv(TOOL_USER_VERBOSE, "YES", 1);
-		break;
+                break;
+            case 's':
+                service_endpoint = strdup(optarg);
+                break;
+            case 'i':
+                id = strdup(optarg);
+                break;
+            case 'v':
+                silent = false;
+                setenv(TOOL_USER_VERBOSE, "YES", 1);
+                break;
             default:
                 print_usage_and_die(stderr);
-        	break;
+            break;
         } // End Switch
     } // End while
 
@@ -109,7 +109,7 @@ int main(int argc, char* argv[]) {
     // -------------------------------------------------------------------------
     if (strlen(argv[optind]) > GLITE_LFN_LENGTH + 6) {
         TRACE_ERR((stderr, "Remote filename is too long (more than %d chars)!\n", GLITE_LFN_LENGTH + 6));
-	return -1;
+        return -1;
     }
     strcpy(remotefilename,argv[optind]);
 
@@ -117,64 +117,57 @@ int main(int argc, char* argv[]) {
 
 
     if(NULL == id) {
-	// Open remote file
-	// -------------------------------------------------------------------------
-	glite_result gl_res;
-	glite_handle fh = glite_open(remotefilename,O_RDONLY,0,0,&gl_res);
-	if (0 == fh) {
-	    const char * error_msg = glite_strerror(gl_res);
-	    TRACE_ERR((stderr,"Cannot Open Remote File %s. Error is \"%s (code: %d)\"\n",remotefilename,error_msg, gl_res));
-	    return -1;
-	}
+        // Open remote file
+        // -------------------------------------------------------------------------
+        glite_result gl_res;
+        glite_handle fh = glite_open(remotefilename,O_RDONLY,0,0,&gl_res);
+        if (0 == fh) {
+            const char * error_msg = glite_strerror(gl_res);
+            TRACE_ERR((stderr,"Cannot Open Remote File %s. Error is \"%s (code: %d)\"\n",remotefilename,error_msg, gl_res));
+            return -1;
+        }
 
-	struct glite_stat stat_buf;
-	glite_int32 result = glite_fstat(fh, &stat_buf);
-	if(0 != result)
-	    {
-		const char * error_msg = glite_strerror(result);
-		TRACE_ERR((stderr, "Cannot Get Remote File Stat. Error is \"%s (code: "
-			   "%d)\"\n",error_msg, result));
-		glite_close(fh);
-		return -1;
-	    }
+        struct glite_stat stat_buf;
+        glite_int32 result = glite_fstat(fh, &stat_buf);
+        if(0 != result)
+        {
+            const char * error_msg = glite_strerror(result);
+            TRACE_ERR((stderr, "Cannot Get Remote File Stat. Error is \"%s (code: "
+                   "%d)\"\n",error_msg, result));
+            glite_close(fh);
+            return -1;
+        }
 
-	id = stat_buf.guid;
+        id = stat_buf.guid;
     }
 
-    // Initialize eds library
+    // Unlink entries in Hydra
     // -------------------------------------------------------------------------
     char *error;
-    EVP_CIPHER_CTX *dctx;
-
-    if (NULL == (dctx = glite_eds_decrypt_init(remotefilename, &error)))
+    if (glite_eds_unregister(id, &error))
     {
-	TRACE_ERR((stderr, "Error during glite_eds_decrypt_init: %s\n",
-	    error));
-	free(error);
-	if (glite_eds_unregister(id, &error))
-	{
-    	    TRACE_ERR((stderr, "Error during glite_eds_unregister: %s\n",
-		 error));
-	    free(error);
-	}
-	return -1;
+        TRACE_ERR((stderr, "Error during glite_eds_unregister: %s\n", error));
+        free(error);
     }
+    return -1;
 
     // Unlink remote file
     // -------------------------------------------------------------------------
     glite_result gl_res = glite_unlink(remotefilename);
     if (GLITE_IO_SUCCESS != gl_res) {
-    	const char * error_msg = glite_strerror(gl_res);
-        TRACE_ERR((stderr,"Cannot Unlink Remote File %s. Error is \"%s (code: %d)\"\n",remotefilename,error_msg, gl_res));
+        const char * error_msg = glite_strerror(gl_res);
+        TRACE_ERR((stderr, "Cannot Unlink Remote File %s. Error is \"%s (code: %d)\"\n",
+            remotefilename, error_msg, gl_res));
         return -1;
     }
     gettimeofday (&abs_stop_time, &tz);
-    float abs_time=((float)((abs_stop_time.tv_sec - abs_start_time.tv_sec) *1000 + (abs_stop_time.tv_usec - abs_start_time.tv_usec) / 1000));
+    float abs_time= ((float)((abs_stop_time.tv_sec - abs_start_time.tv_sec) *1000 
+        + (abs_stop_time.tv_usec - abs_start_time.tv_usec) / 1000));
         
     TRACE_LOG((stdout,"\nUnlink Completed:\n\n"));
     TRACE_LOG((stdout,"  File          : %s  \n",remotefilename));
-    if (abs_time!=0) {
-    TRACE_LOG((stdout,"  Time [s]      : %f  \n",abs_time/1000.0));
+    if (abs_time != 0) {
+        TRACE_LOG((stdout,"  Time [s]      : %f  \n",abs_time/1000.0));
     }
     TRACE_LOG((stdout,"\n"));
     
