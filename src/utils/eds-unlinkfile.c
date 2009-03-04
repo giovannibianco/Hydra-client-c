f32ab188391c32a62b2989cdd99eddc883fb02b2
akrueger <akrueger@4c42d608-eb89-46b6-9e62-40a981e1fec6>
2009-03-04 12:39:40 +0000

    Fixed. Improved handling of the file name.

--- src/utils/eds-unlinkfile.c
+++ src/utils/eds-unlinkfile.c
@@ -27,6 +27,7 @@
 
 #include <glite/data/hydra/c/eds-simple.h>
 #include <gfal_api.h>
+#include <gfal_internals.h> /* without warranty */
 
 
 #define PROGNAME     "glite-eds-rm"
@@ -36,8 +37,8 @@
 
 #define TOOL_USER_VERBOSE   "__GLITE_EDS_VERBOSE"
 
-#define true	1
-#define false	0
+#define true 1
+#define false 0
 
 
 #define TRACE_LOG(a)  if(!silent) fprintf a
@@ -53,23 +54,22 @@ static void print_usage_and_die(FILE * out){
     fprintf (out, "  -q      : quiet mode\n");
     fprintf (out, "  -v      : verbose mode\n");
     fprintf (out, "  -V      : print version and exit\n");
-    if(out == stdout){
-        exit(0);
-    }
-    exit(-1);
+
+    exit((out == stdout) ? 0 : -1);
 }
 
 int main(int argc, char* argv[]) {
 
-    char   remotefilename[GFAL_LFN_LENGTH + 7];
+    char remotefilename[GFAL_LFN_LENGTH + 7];
+    char errbuf[256];
 
     struct timeval abs_start_time;
     struct timeval abs_stop_time;
     struct timezone tz;
 
     char *id = NULL;
-    int silent     = false;
-    
+    int silent = false;
+
     int flag;
     while ((flag = getopt (argc, argv, "qhvVi:")) != -1) {
         switch (flag) {
@@ -82,15 +82,20 @@ int main(int argc, char* argv[]) {
                 break;
             case 'i':
                 id = strdup(optarg);
+                if (id == NULL) {
+                    TRACE_ERR((stderr, "Failed duplicate -i argument, parameter %d chars\n",
+                            strlen(optarg)));
+                    exit(-1);
+                }
                 break;
             case 'v':
                 silent = false;
                 setenv(TOOL_USER_VERBOSE, "YES", 1);
                 break;
             case 'V':
-				fprintf (stdout, "<%s> Version %s by %s\n",
-					PROGNAME, PACKAGE_VERSION, PROGAUTHOR);
-				exit(0);
+                fprintf (stdout, "<%s> Version %s by %s\n",
+                        PROGNAME, PACKAGE_VERSION, PROGAUTHOR);
+                exit(0);
             default:
                 print_usage_and_die(stderr);
             break;
@@ -101,60 +106,84 @@ int main(int argc, char* argv[]) {
         print_usage_and_die(stderr);
     }
 
-    // Initialize the client
+    // Copy file name
     // -------------------------------------------------------------------------
-    
-    // Check if remote file name format: 
-    //   Must be lfn://<filename> or guid://<guid>
-    // TODO
-    // -------------------------------------------------------------------------
-    if (strlen(argv[optind]) > GFAL_LFN_LENGTH + 6) {
-        TRACE_ERR((stderr, "Remote filename is too long (more than %d chars)!\n", GFAL_LFN_LENGTH + 6));
-        return -1;
-    }
     strcpy(remotefilename,argv[optind]);
+    if (canonical_url(argv[optind], "lfn", remotefilename, sizeof(remotefilename),
+                errbuf, sizeof(errbuf)) < 0) {
+            TRACE_ERR((stderr,"Error in Remote File Name %s. Error is %s (code: %d)\"\n",
+                    remotefilename, errbuf, errno));
+        goto err;
+    }
+
 
     gettimeofday(&abs_start_time,&tz);
 
-
-    if(NULL == id) {
-	// Open remote file
+    // Fetch the guid of the file
     // -------------------------------------------------------------------------
-    char errbuf[256];
-	if((id = guidfromlfn(remotefilename, errbuf, sizeof(errbuf))) == NULL){
-		TRACE_ERR((stderr,"Cannot get guid for remote File %s. Error is %s (code: %d)\"\n",remotefilename,errbuf,errno));   	  
-		return (-1);
-	  }
+    if(id == NULL) {
+        if (strncmp(remotefilename, "lfn:", 4) == 0) {
+            if ((id = guidfromlfn(remotefilename + 4, errbuf, sizeof(errbuf))) == NULL) {
+                TRACE_ERR((stderr,"Cannot get guid for LFN-file %s. Error is %s (code: %d)\"\n",
+                            remotefilename + 4, errbuf, errno));
+                goto err;
+            }
+        } else if (strncmp(remotefilename, "guid:", 5) == 0) {
+            if ((id = strdup(remotefilename + 5)) == NULL) {
+                TRACE_ERR((stderr,"Failed duplicate guid, length %d\n",
+                            strlen(remotefilename + 5)));
+                goto err;
+            }
+        } else {
+            TRACE_ERR((stderr,"Protocol not supported: %s. Use LFN- or GUID-format.\n",
+                        remotefilename));
+            goto err;
+        }
     }
 
     // Unlink entries in Hydra
     // -------------------------------------------------------------------------
+    int failures = 0;
     char *error;
     if (glite_eds_unregister(id, &error))
     {
-        TRACE_ERR((stderr, "Error during glite_eds_unregister: %s\n", error));
+        TRACE_ERR((stderr, "WARNING: during glite_eds_unregister: %s\n", error));
         free(error);
+        failures++;
     }
-    return -1;
 
     // Unlink remote file
     // -------------------------------------------------------------------------
-    
-	if(gfal_unlink(remotefilename)<0) {
-		TRACE_ERR((stderr,"Warning: cannot unlink remote file %s. Error is %s (code: %d)\"\n",remotefilename,strerror(errno),errno));
-	}
+
+    if (gfal_unlink(remotefilename)<0) {
+        TRACE_ERR((stderr,"WARNING: cannot unlink remote file %s. Error is %s (code: %d)\"\n",
+                    remotefilename, strerror(errno), errno));
+        failures++;
+    }
+
+    if (failures > 1)
+        goto err;
 
     gettimeofday (&abs_stop_time, &tz);
     float abs_time= ((float)((abs_stop_time.tv_sec - abs_start_time.tv_sec) *1000 
         + (abs_stop_time.tv_usec - abs_start_time.tv_usec) / 1000));
-        
-    TRACE_LOG((stdout,"\nUnlink Completed:\n\n"));
-    TRACE_LOG((stdout,"  File          : %s  \n",remotefilename));
-    if (abs_time != 0) {
-        TRACE_LOG((stdout,"  Time [s]      : %f  \n",abs_time/1000.0));
+
+    if (!silent) {
+        TRACE_LOG((stdout,"\nUnlink Completed:\n\n"));
+        TRACE_LOG((stdout,"  File          : %s  \n",remotefilename));
+        if (abs_time != 0) {
+            TRACE_LOG((stdout,"  Time [s]      : %f  \n",abs_time/1000.0));
+        }
+        TRACE_LOG((stdout,"\n"));
     }
-    TRACE_LOG((stdout,"\n"));
-        
-    
+
     return 0;
+
+    // Error handling
+    // -------------------------------------------------------------------------
+
+err:
+    return -1;
 }
+
+/* vim: set et sw=4 ts=4: */
